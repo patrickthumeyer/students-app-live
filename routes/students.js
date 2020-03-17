20239cb5162a18c83dd89a7772f9552bc0748f48
patrickthumeyer <p.thumeyer@gmail.com>
2020-03-17 13:49:19 +0100

    implemented filesystem in all methods, not sure if correctly

--- routes/students.js
+++ routes/students.js
@@ -3,61 +3,88 @@ const router = express.Router();
 const fs = require("fs");
 const path = require("path");
 const myPath = path.join(__dirname, "../data/students.json");
+const Joi = require("@hapi/joi");
 
 // - GET (all, individual)
 router.get("/", (req, res) => {
   fs.readFile(myPath, "utf-8", (err, data) => {
     if (err) throw err;
-    res.status(200).json(students);
+    res.status(200).json(JSON.parse(data));
   });
 });
 
 router.get("/:name", (req, res) => {
-  const student = students.find(
-    ({ name }) => name.toLowerCase() === req.params.name.toLowerCase()
-  );
+  fs.readFile(myPath, "utf-8", (err, data) => {
+    if (err) throw err;
 
-  if (student) {
-    return res.status(200).json(student);
-  }
+    data = JSON.parse(data);
 
-  res.status(404).json({ error: "Student not found" });
+    const student = data.find(
+      ({ name }) => name.toLowerCase() === req.params.name.toLowerCase()
+    );
+
+    if (student) {
+      return res.status(200).json(student);
+    }
+
+    res.status(404).json({ error: "Student not found" });
+  });
 });
 
 // - PUT (individual)
 router.put("/:name", (req, res) => {
-  if (req.params.name && req.body) {
-    students = students.map(student => {
-      if (student.name.toLowerCase() === req.params.name.toLowerCase()) {
-        Object.assign(student, req.body);
-      }
+  fs.writeFile(myPath, "utf-8", (err, data) => {
+    if (err) throw err;
 
-      return student;
-    });
-  }
-  res.send(students);
+    data = JSON.parse(data);
+
+    if (req.params.name && req.body) {
+      data = data.map(student => {
+        if (student.name.toLowerCase() === req.params.name.toLowerCase()) {
+          Object.assign(student, req.body);
+        }
+
+        return student;
+      });
+    }
+    res.send(data);
+  });
 });
+
 // - DELETE (individual)
 router.delete("/:name", (req, res) => {
-  if (req.params.name) {
-    students = students.filter(
-      ({ name }) => name.toLowerCase() !== req.params.name.toLowerCase()
-    );
-  }
+  fs.writeFile(myPath, "utf-8", (err, data) => {
+    if (err) throw err;
 
-  res.send(students);
+    data = JSON.parse(data);
+
+    if (req.params.name) {
+      data = data.filter(
+        ({ name }) => name.toLowerCase() !== req.params.name.toLowerCase()
+      );
+    }
+
+    res.send(data);
+  });
 });
+
 // - POST (individual)
 router.post("/", (req, res) => {
-  if (req.body) {
-    students.push(req.body);
-    return res.send({
-      status: "success",
-      message: `student with name: ${req.body.name} added`
-    });
-  }
+  fs.writeFile(myPath, "utf-8", (err, data) => {
+    if (err) throw err;
 
-  res.send("NO!");
+    data = JSON.parse(data);
+
+    if (req.body) {
+      data.push(req.body);
+      return res.send({
+        status: "success",
+        message: `student with name: ${req.body.name} added`
+      });
+    }
+
+    res.send("NO!");
+  });
 });
 
 module.exports = router;
